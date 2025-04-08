dc26ed4c4c18fc4b1d02e9087d941733155c269f
Oscar Falk <oscar.falk@gmail.com>
2025-04-08 14:01:49 +0200

    feat: add navigation item function component
    create global header file and function heads for nav item, site navigation list, external navigation list and global header
    
    write navigation item function component

---
+++ components/navigation/global-header.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+export function NavItem({children}:{children:ReactNode}){
+    return(
+        <li className="nav-item">
+            {children}
+        </li>
+    );
+}
+
+export function SiteNavList(){
+    
+}
+
+export function ExtNavList(){
+
+}
+
+export function GlobalHeader(){
+
+}
