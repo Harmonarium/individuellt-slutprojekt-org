d51c270087d172f37a02862edb6f2f538e94aa56
Oscar Falk <oscar.falk@gmail.com>
2025-04-09 10:37:04 +0200

    feat: change font to inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { GlobalHeader } from "@/components/navigation/global-header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
@@ -27,10 +22,9 @@ export default function RootLayout({
     <html lang="en">
       
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <GlobalHeader />
-          {/* <h1>Site-header</h1><ul className="flex gap-4"><li>home</li><li>showcase</li><li>projects</li><li><div className="bg-amber-300">github</div></li><li><div className="bg-amber-300">linkedin</div></li></ul> */}
         {children}
       </body>
     </html>
