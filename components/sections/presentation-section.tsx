3ba7fb5734c05d4e9bcce84631a3991b66175b92
Oscar Falk <oscar.falk@gmail.com>
2025-04-14 16:55:34 +0200

    feat: add fetch function for image url for presentation section

--- components/sections/presentation-section.tsx
+++ components/sections/presentation-section.tsx
@@ -10,7 +10,7 @@ export default function PresentationSection(){
                 <Image className="bg-zinc-300" src='/vercel.svg' alt='me' width={100} height={175}/>
                 <article className="presentation-article">
                     <h3>My presentation</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.}</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </article>
             </div>  
             <article className="background-article">
--- server_actions/server-actions.ts
+++ server_actions/server-actions.ts
@@ -3,11 +3,10 @@ import { supabase } from "./supabase";
 export async function fetchPresentationContent(){
     let {data, error} = await supabase.from('Presentation').select('Content(text)').eq('section_name','Presentation');
     if(error){
-        console.log("Error: ", error);
+        console.err("Error: ", error);
     }
     else{
         if(data!=null){
-            console.log(data);
             let obj=data[0];
             let content=obj.Content;
             let text=content.text;
@@ -23,16 +22,30 @@ export async function fetchPresentationContent(){
 export async function fetchBackgroundContent(){
     let {data, error} = await supabase.from('Presentation').select('Content(text)').eq('section_name','Background');
     if(error){
-        console.log("Error: ", error);
+        console.err("Error: ", error);
     }
     else{
         if(data!=null){
-            console.log(data);
             let obj=data[0];
             let content=obj.Content;
             let text=content.text;
             return text;
         }
             
+    }
+}
+
+export async function fetchPresentationImageURL(){
+    let {data, error} = await supabase.from('Presentation').select('Image(image_url)').eq('section_name','Photo');
+    if(error){
+        console.err("Error: ", error);
+    }
+    else{
+        if(data){
+            let obj=data[0];
+            let image=obj.Image;
+            let image_url=image.image_url;
+            return image_url;
+        }
     }
 }
