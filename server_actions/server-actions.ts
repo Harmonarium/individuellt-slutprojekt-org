c06f57930711d65ee6e09d6b25c44e7cc3d9cad5
Oscar Falk <oscar.falk@gmail.com>
2025-04-14 16:57:46 +0200

    fix: change typo of err where should be error in server actions

--- server_actions/server-actions.ts
+++ server_actions/server-actions.ts
@@ -3,7 +3,7 @@ import { supabase } from "./supabase";
 export async function fetchPresentationContent(){
     let {data, error} = await supabase.from('Presentation').select('Content(text)').eq('section_name','Presentation');
     if(error){
-        console.err("Error: ", error);
+        console.error("Error: ", error);
     }
     else{
         if(data!=null){
@@ -22,7 +22,7 @@ export async function fetchPresentationContent(){
 export async function fetchBackgroundContent(){
     let {data, error} = await supabase.from('Presentation').select('Content(text)').eq('section_name','Background');
     if(error){
-        console.err("Error: ", error);
+        console.error("Error: ", error);
     }
     else{
         if(data!=null){
@@ -38,7 +38,7 @@ export async function fetchBackgroundContent(){
 export async function fetchPresentationImageURL(){
     let {data, error} = await supabase.from('Presentation').select('Image(image_url)').eq('section_name','Photo');
     if(error){
-        console.err("Error: ", error);
+        console.error("Error: ", error);
     }
     else{
         if(data){
