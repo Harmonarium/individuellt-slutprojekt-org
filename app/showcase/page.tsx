0f275d007234984e8b528e307e4a2b05603154a0
Oscar Falk <oscar.falk@gmail.com>
2025-04-16 14:00:42 +0200

    feat: improve styling

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -6,25 +6,43 @@ import { ReactElement } from "react";
 
 function generateShowcases():ShowcaseItem[]{
     let showcases:ShowcaseItem[] = [];
-    let et01:ReactElement<HTMLDivElement>=<div id="dp01_et01" className="showcase-box bg-blue-400 w-200 h-200"></div>;
+    let et01:ReactElement<HTMLDivElement>=<div id="dp01_et01" className="showcase-box bg-blue-400 w-20 h-20"></div>;
     const eH01=()=>{
         const et=document.getElementById("dp01_et01");
         if(et){
-            et.classList.replace("bg-blue-400","bg-zinc-400");
+            /* et.classList.replace("bg-blue-400","bg-zinc-400"); */
             et.style.backgroundColor="yellow";    
         }
             
     }
-    let ip01:ReactElement<HTMLInputElement>=<input id="cp01_ip01" className="showcase-input-button" type="button" onClick={eH01} value="Change Color"/>;
+    const eH02=()=>{
+        const et=document.getElementById("dp01_et01");
+        if(et){
+            /* et.classList.replace("bg-blue-400","bg-zinc-400"); */
+            et.style.backgroundColor="blue";    
+        }
+            
+    }
+    const eH03=()=>{
+        const et=document.getElementById("dp01_et01");
+        if(et){
+            /* et.classList.replace("bg-blue-400","bg-zinc-400"); */
+            et.style.backgroundColor="red";    
+        }
+            
+    }
+    let ip01:ReactElement<HTMLInputElement>=<input id="cp01_ip01" className="showcase-input-button" type="button" onClick={eH01} value="Yellow"/>;
+    let ip02:ReactElement<HTMLInputElement>=<input id="cp01_ip02" className="showcase-input-button" type="button" onClick={eH02} value="blue"/>;
+    let ip03:ReactElement<HTMLInputElement>=<input id="cp01_ip03" className="showcase-input-button" type="button" onClick={eH03} value="red"/>;
 
     let showcase:ShowcaseItem = {
         title:"test",
         imageURL:"/vercel.svg",
-        description:"Testshowcase. Press button to change color",
-        inputs: [ip01],
-        eventHandlers: [eH01],
+        description:"Testshowcase, press button to change color",
+        inputs: [ip01, ip02, ip03],
+        eventHandlers: [eH01,eH02,eH03],
         eventTargets: [et01],
-        controlPanel: <div id="cp01" className="control-panel">{ip01}</div>,
+        controlPanel: <div id="cp01" className="control-panel">{ip01}{ip02}{ip03}</div>,
         displayPanel: <div id="dp01" className="display-panel">{et01}</div>
     }
 
