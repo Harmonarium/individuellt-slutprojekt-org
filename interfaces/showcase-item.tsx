80a60f788ee4659eae42169f71f2eeb0972dce21
Oscar Falk <oscar.falk@gmail.com>
2025-04-17 12:25:01 +0200

    feat: add JSON interfaces and template

--- interfaces/showcase-item.tsx
+++ interfaces/showcase-item.tsx
@@ -2,6 +2,24 @@ import { ReactNode } from "react";
 
 export type showcaseEventHandler = () => void;
 
+export interface JSONfunction{
+    function: {
+        arguments:string;
+        body:string;
+    }
+}
+
+export interface JSONShowcaseItem{
+    displayPanel: string;
+    controlPanel: string;
+    eventTargetIDs: string[];
+    inputIDs: string[];
+    eventHandlers: JSONfunction[];
+    description: string;
+    title:string;
+    imageURL:string;
+}
+
 export interface ShowcaseItem{
     displayPanel: ReactNode;
     controlPanel: ReactNode;
